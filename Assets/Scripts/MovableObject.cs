856aef810624ce156d7f212e3261f836c3d3a984
Joshua D. Lai <jdlai@cpp.edu>
2024-03-19 18:03:30 -0700

    adjust movable

--- Assets/Scripts/MovableObject.cs
+++ Assets/Scripts/MovableObject.cs
@@ -5,12 +5,14 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class MovableObject : MonoBehaviour
 {
+    XRSimpleInteractable interact;
     XRGrabInteractable _grabInteractable;
     private void Start()
     {
         _grabInteractable = GetComponent<XRGrabInteractable>();
         EventBus.instance.OnStartAttempt.AddListener(AttemptStarted);
         EventBus.instance.OnStopAttempt.AddListener(AttemptStopped);
+        interact.selectEntered.Invoke(null);
     }
     void AttemptStarted()
     {
