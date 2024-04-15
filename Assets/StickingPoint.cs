d6c4b05b98989dbac19c6804f270be29c17eee99
OrbinWaffle <joshlaiguy@gmail.com>
2024-04-15 16:09:17 -0700

    strut testing

---
+++ Assets/StickingPoint.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickingPoint : MonoBehaviour
+{
+    Collider _collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Stick()
+    {
+
+    }
+    public void Unstick()
+    {
+        
+    }
+}
