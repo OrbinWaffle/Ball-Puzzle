3aaf60cc9ef2632114460d7e743d8d89b066a139
OrbinWaffle <joshlaiguy@gmail.com>
2024-04-16 15:42:50 -0700

    more strut experimentation

--- Assets/Scripts/Ensmallenator.cs
+++ Assets/Scripts/Ensmallenator.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Enbiggenator : MachineBase
+public class Ensmallenator : MachineBase
 {
     public Transform exit;
     protected override void ActivateMachine(Collider col)
     {
         base.ActivateMachine(col);
         col.transform.position = exit.transform.position;
-        col.transform.localScale = Vector3.one * 4;
+        col.transform.localScale = Vector3.one;
     }
 }
